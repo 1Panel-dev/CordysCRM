8f8b19461d3e22c17a74a8a120ee8d371cfd21e9
song-cc-rock <changchang.song@fit2cloud.com>
2025-11-11 11:46:24 +0800

    refactor: Link scenario
    --story=1020103 --user=宋昌昌 表单联动优化 https://www.tapd.cn/34675357/s/1796711

---
+++ backend/crm/src/main/java/cn/cordys/common/constants/LinkScenario.java
@@ -0,0 +1,18 @@
+package cn.cordys.common.constants;
+
+/**
+ * 联动场景
+ *
+ * @author song-cc-rock
+ */
+public enum LinkScenario {
+
+	/**
+	 * 线索转客户
+	 */
+	CLUE_TO_CUSTOMER,
+	/**
+	 * 线索转商机
+	 */
+	CLUE_TO_OPPORTUNITY,
+}
