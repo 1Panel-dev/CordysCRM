ad79bfa901bbee66491d5c782622f4a4e36b5b32
WangXu10 <xu.wang@fit2cloud.com>
2025-11-19 11:02:17 +0800

    feat: init contract module

--- backend/crm/src/main/resources/migration/1.4.0/dml/V1.4.0_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.4.0/dml/V1.4.0_2_1__data.sql
@@ -2,7 +2,8 @@
 
 SET SESSION innodb_lock_wait_timeout = 7200;
 
-
+insert into sys_module value (UUID_SHORT(), '100001', 'contract', false, 9,
+                              'admin', UNIX_TIMESTAMP() * 1000, 'admin', UNIX_TIMESTAMP() * 1000);
 
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
