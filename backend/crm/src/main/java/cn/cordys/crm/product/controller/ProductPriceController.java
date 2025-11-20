b460168978f4614dd5f74613dcfd5059adce446e
song-cc-rock <changchang.song@fit2cloud.com>
2025-11-20 17:22:15 +0800

    feat: Price crud

--- backend/crm/src/main/java/cn/cordys/crm/product/controller/ProductPriceController.java
+++ backend/crm/src/main/java/cn/cordys/crm/product/controller/ProductPriceController.java
@@ -1,10 +1,14 @@
 package cn.cordys.crm.product.controller;
 
 import cn.cordys.common.constants.PermissionConstants;
+import cn.cordys.common.pager.PagerWithOption;
 import cn.cordys.context.OrganizationContext;
 import cn.cordys.crm.product.domain.ProductPrice;
 import cn.cordys.crm.product.dto.request.ProductPriceAddRequest;
 import cn.cordys.crm.product.dto.request.ProductPriceEditRequest;
+import cn.cordys.crm.product.dto.request.ProductPricePageRequest;
+import cn.cordys.crm.product.dto.response.ProductPriceGetResponse;
+import cn.cordys.crm.product.dto.response.ProductPriceResponse;
 import cn.cordys.crm.product.service.ProductPriceService;
 import cn.cordys.security.SessionUtils;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,10 +16,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author song-cc-rock
@@ -27,6 +30,13 @@ public class ProductPriceController {
 
 	@Resource
 	private ProductPriceService priceService;
+
+	@PostMapping("/page")
+	@RequiresPermissions(PermissionConstants.PRICE_READ)
+	@Operation(summary = "价格列表")
+	public PagerWithOption<List<ProductPriceResponse>> list(@Validated @RequestBody ProductPricePageRequest request) {
+		return priceService.list(request, OrganizationContext.getOrganizationId());
+	}
 
 	@PostMapping("/add")
 	@RequiresPermissions(PermissionConstants.PRICE_ADD)
@@ -42,4 +52,17 @@ public class ProductPriceController {
 		return priceService.update(request, SessionUtils.getUserId(), OrganizationContext.getOrganizationId());
 	}
 
+	@GetMapping("/get/{id}")
+	@RequiresPermissions(PermissionConstants.PRICE_READ)
+	@Operation(summary = "价格表详情")
+	public ProductPriceGetResponse get(@PathVariable String id) {
+		return priceService.get(id);
+	}
+
+	@GetMapping("/delete/{id}")
+	@RequiresPermissions(PermissionConstants.PRICE_DELETE)
+	@Operation(summary = "删除价格表")
+	public void delete(@PathVariable String id) {
+		priceService.delete(id);
+	}
 }
