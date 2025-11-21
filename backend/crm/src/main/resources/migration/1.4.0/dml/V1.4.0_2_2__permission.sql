4ee782b0efae2b82e2b260cfb50f364b7424901b
guoyuqi <xiaomeinvG@126.com>
2025-11-21 19:06:05 +0800

    feat: quotation service

--- backend/crm/src/main/resources/migration/1.4.0/dml/V1.4.0_2_2__permission.sql
+++ backend/crm/src/main/resources/migration/1.4.0/dml/V1.4.0_2_2__permission.sql
@@ -1,5 +1,12 @@
 -- set innodb lock wait timeout
 SET SESSION innodb_lock_wait_timeout = 7200;
 
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:READ');
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:ADD');
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:UPDATE');
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:DELETE');
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:EXPORT');
+INSERT INTO sys_role_permission (id, role_id, permission_id) VALUES (UUID_SHORT(), 'org_admin', 'OPPORTUNITY_QUOTATION:APPROVAL');
+
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
