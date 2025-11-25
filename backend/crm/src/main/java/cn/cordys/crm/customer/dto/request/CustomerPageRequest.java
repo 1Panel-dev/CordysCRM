604a67604dd218b8d7928c59d38610baa4ce71b9
song-cc-rock <changchang.song@fit2cloud.com>
2025-11-25 14:45:19 +0800

    fix: Related customer list query
    --bug=1063793 --user=宋昌昌 【线索】github#413线索关联客户时，带出来所有客户 https://www.tapd.cn/34675357/s/1802881

--- backend/crm/src/main/java/cn/cordys/crm/customer/dto/request/CustomerPageRequest.java
+++ backend/crm/src/main/java/cn/cordys/crm/customer/dto/request/CustomerPageRequest.java
@@ -1,6 +1,7 @@
 package cn.cordys.crm.customer.dto.request;
 
 import cn.cordys.common.dto.BasePageRequest;
+import cn.cordys.common.dto.DeptDataPermissionDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -17,9 +18,12 @@ public class CustomerPageRequest extends BasePageRequest {
     @Schema(description = "公海ID{公海客户列表时传参}")
     private String poolId;
 
-    @Schema(description = "是否查询转移客户列表")
+    @Schema(description = "是否查询关联客户列表")
     private Boolean transition;
 
-    @Schema(description = "查询转移需要的公海客户时传参")
+    @Schema(description = "查询需要关联的公海客户传参")
     private List<String> transitionPoolIds;
+
+	@Schema(description = "关联客户数据权限")
+	private DeptDataPermissionDTO transitionDataPermission;
 }
