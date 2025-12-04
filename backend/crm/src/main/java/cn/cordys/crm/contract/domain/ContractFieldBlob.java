b590d5689b4a14b7b635efc001d6f41bcd352b4d
guoyuqi <xiaomeinvG@126.com>
2025-12-04 14:33:14 +0800

    refactor: contract blob extend

--- backend/crm/src/main/java/cn/cordys/crm/contract/domain/ContractFieldBlob.java
+++ backend/crm/src/main/java/cn/cordys/crm/contract/domain/ContractFieldBlob.java
@@ -1,12 +1,12 @@
 package cn.cordys.crm.contract.domain;
 
-import cn.cordys.common.domain.BaseResourceField;
+import cn.cordys.common.domain.BaseResourceSubField;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 
 @Data
 @Table(name = "contract_field_blob")
-public class ContractFieldBlob extends BaseResourceField {
+public class ContractFieldBlob extends BaseResourceSubField {
 
 }
