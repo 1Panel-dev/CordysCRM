0c6023ac47fd449bfd6d7aff79a94a65c3ac12be
song-cc-rock <changchang.song@fit2cloud.com>
2025-12-09 17:24:57 +0800

    fix: Log detail error
    --bug=1064334 --user=宋昌昌 【价格表】价格表修改状态后操作日志中状态显示不正确 https://www.tapd.cn/34675357/s/1808479

--- backend/crm/src/main/java/cn/cordys/crm/product/service/PriceLogService.java
+++ backend/crm/src/main/java/cn/cordys/crm/product/service/PriceLogService.java
@@ -1,18 +1,31 @@
 package cn.cordys.crm.product.service;
 
+import cn.cordys.common.constants.BusinessModuleField;
 import cn.cordys.common.constants.FormKey;
 import cn.cordys.common.dto.JsonDifferenceDTO;
 import cn.cordys.common.util.Translator;
+import cn.cordys.context.OrganizationContext;
+import cn.cordys.crm.system.dto.field.base.BaseField;
+import cn.cordys.crm.system.dto.field.base.HasOption;
+import cn.cordys.crm.system.dto.field.base.OptionProp;
+import cn.cordys.crm.system.dto.response.ModuleFormConfigDTO;
 import cn.cordys.crm.system.service.BaseModuleLogService;
+import cn.cordys.crm.system.service.ModuleFormCacheService;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.Strings;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class PriceLogService extends BaseModuleLogService {
+
+	@Resource
+	private ModuleFormCacheService moduleFormCacheService;
 
     @Override
     public List<JsonDifferenceDTO> handleLogField(List<JsonDifferenceDTO> differences, String orgId) {
@@ -21,7 +34,9 @@ public class PriceLogService extends BaseModuleLogService {
         List<JsonDifferenceDTO> toRemove = new ArrayList<>();
 
         for (JsonDifferenceDTO differ : differences) {
-
+			if (Strings.CS.equals(differ.getColumn(), BusinessModuleField.PRICE_STATUS.getBusinessKey())) {
+				setPriceStatus(differ, orgId);
+			}
             if (differ.getColumn().contains(Translator.get("products_info"))) {
                 differ.setColumnName(differ.getColumn());
             }
@@ -35,4 +50,21 @@ public class PriceLogService extends BaseModuleLogService {
     }
 
 
+	/**
+	 * 产品
+	 *
+	 * @param differ businessModuleField
+	 */
+	private void setPriceStatus(JsonDifferenceDTO differ, String orgId) {
+		ModuleFormConfigDTO priceConfig = moduleFormCacheService.getBusinessFormConfig(FormKey.PRICE.getKey(), orgId);
+		Optional<BaseField> statusField = priceConfig.getFields().stream().filter(f -> Strings.CS.equals(BusinessModuleField.PRICE_STATUS.getKey(), f.getInternalKey())).findFirst();
+		if (statusField.isEmpty()) {
+			return;
+		}
+		List<OptionProp> options = ((HasOption) statusField.get()).getOptions();
+		differ.setNewValueName(options.stream()
+				.filter(optionProp -> Strings.CS.equals(optionProp.getValue(), differ.getNewValue().toString())).map(OptionProp::getLabel));
+		differ.setOldValueName(options.stream()
+				.filter(optionProp -> Strings.CS.equals(optionProp.getValue(), differ.getOldValue().toString())).map(OptionProp::getLabel));
+	}
 }
