69eb471834b487a786cf3533fc2701022c6e9b10
song-cc-rock <changchang.song@fit2cloud.com>
2025-12-12 19:04:07 +0800

    fix: Price export error
    --bug=1064536 --user=宋昌昌 【产品】价格表-导出勾选价格表-选择全部字段导出失败 https://www.tapd.cn/34675357/s/1811105

--- backend/crm/src/main/java/cn/cordys/crm/system/excel/handler/SummaryMergeHandler.java
+++ backend/crm/src/main/java/cn/cordys/crm/system/excel/handler/SummaryMergeHandler.java
@@ -22,41 +22,43 @@ public record SummaryMergeHandler(List<int[]> mergeRegions, List<Integer> mergeC
 		}
 
 		// 筛选汇总列
-		List<Integer> summaryCols = mergeColumns.stream().filter(col -> {
-					Cell cell = sheet.getRow(offset - 1).getCell(col);
-					if (cell == null) {
-						return false;
-					}
-					String head = cell.getStringCellValue();
-					return head != null && head.startsWith(Translator.get("sum"));
-				}).toList();
-
-		// 合并行区域内的汇总列的值
-		for (int[] region : mergeRegions) {
-			int start = region[0] + offset;
-			int end = region[1] + offset;
-			for (Integer colIndex : summaryCols) {
-				// 合计
-				BigDecimal total = BigDecimal.ZERO;
-				for (int r = start; r <= end; r++) {
-					Cell cell = sheet.getRow(r).getCell(colIndex);
-					if (cell != null) {
-						String val = String.valueOf(cell.getNumericCellValue());
-						if (StringUtils.isNotEmpty(val) && NumberUtils.isParsable(val)) {
-							total = total.add(new BigDecimal(val));
+		Row headRow = sheet.getRow(offset - 1);
+		if (headRow != null) {
+			List<Integer> summaryCols = mergeColumns.stream().filter(col -> {
+				Cell cell = headRow.getCell(col);
+				if (cell == null) {
+					return false;
+				}
+				String head = cell.getStringCellValue();
+				return head != null && head.startsWith(Translator.get("sum"));
+			}).toList();
+			// 合并行区域内的汇总列的值
+			for (int[] region : mergeRegions) {
+				int start = region[0] + offset;
+				int end = region[1] + offset;
+				for (Integer colIndex : summaryCols) {
+					// 合计
+					BigDecimal total = BigDecimal.ZERO;
+					for (int r = start; r <= end; r++) {
+						Cell cell = sheet.getRow(r).getCell(colIndex);
+						if (cell != null) {
+							String val = String.valueOf(cell.getNumericCellValue());
+							if (StringUtils.isNotEmpty(val) && NumberUtils.isParsable(val)) {
+								total = total.add(new BigDecimal(val));
+							}
 						}
 					}
-				}
-				// 只写入第一行, 清空其他行的汇总值 (不同值合并展示有误)
-				Cell sumCell = sheet.getRow(start).getCell(colIndex);
-				sumCell.setCellValue(total.toPlainString());
-				Workbook wb = sheet.getWorkbook();
-				CellStyle style = wb.createCellStyle();
-				style.setAlignment(HorizontalAlignment.RIGHT);
-				style.setVerticalAlignment(VerticalAlignment.CENTER);
-				sumCell.setCellStyle(style);
-				for (int r = start + 1; r <= end; r++) {
-					sheet.getRow(r).getCell(colIndex).setCellValue("");
+					// 只写入第一行, 清空其他行的汇总值 (不同值合并展示有误)
+					Cell sumCell = sheet.getRow(start).getCell(colIndex);
+					sumCell.setCellValue(total.toPlainString());
+					Workbook wb = sheet.getWorkbook();
+					CellStyle style = wb.createCellStyle();
+					style.setAlignment(HorizontalAlignment.RIGHT);
+					style.setVerticalAlignment(VerticalAlignment.CENTER);
+					sumCell.setCellStyle(style);
+					for (int r = start + 1; r <= end; r++) {
+						sheet.getRow(r).getCell(colIndex).setCellValue("");
+					}
 				}
 			}
 		}
