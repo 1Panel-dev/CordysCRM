fec35ccbf2eaf60f05edf4dbb9d6ac05ca8e8d52
WangXu10 <xu.wang@fit2cloud.com>
2025-12-23 10:25:44 +0800

    refactor: contract stage optimization
    --story=1020485 --user=王旭 【合同】合同阶段优化 https://www.tapd.cn/34675357/s/1816486

---
+++ backend/crm/src/main/resources/migration/1.4.1/dml/V1.4.1_2_2__modify_tel.sql
@@ -0,0 +1,10 @@
+-- set innodb lock wait timeout
+SET SESSION innodb_lock_wait_timeout = 7200;
+
+
+DELETE FROM sys_role_permission WHERE permission_id in ('CONTRACT:ARCHIVE', 'CONTRACT:VOIDED');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT:STAGE');
+
+SET SESSION innodb_lock_wait_timeout = DEFAULT;
