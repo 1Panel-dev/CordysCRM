7516d8531e455ccf1daebce6a7c1b8ad63568c12
WangXu10 <xu.wang@fit2cloud.com>
2025-12-25 10:39:28 +0800

    refactor: contract view field

--- backend/crm/src/main/resources/migration/1.4.1/dml/V1.4.1_2_2__modify_tel.sql
+++ backend/crm/src/main/resources/migration/1.4.1/dml/V1.4.1_2_2__modify_tel.sql
@@ -12,4 +12,12 @@ DELETE FROM sys_user_view_condition WHERE name = 'archivedStatus';
 INSERT INTO sys_role_permission (id, role_id, permission_id)
 VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT:DELETE');
 
+
+UPDATE sys_user_view_condition suc
+    JOIN sys_user_view su
+ON suc.sys_user_view_id = su.id
+    SET suc.NAME = 'stage'
+WHERE suc.NAME = 'status'
+  AND su.resource_type = 'CONTRACT';
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
