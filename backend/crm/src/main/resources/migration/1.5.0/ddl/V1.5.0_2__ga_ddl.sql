4729e513e78e19f17bd80753e952dde41848725f
zhao <70558837+fit2-zhao@users.noreply.github.com>
2025-12-30 10:36:32 +0800

    refactor: standardize SQL formatting

--- backend/crm/src/main/resources/migration/1.5.0/ddl/V1.5.0_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.5.0/ddl/V1.5.0_2__ga_ddl.sql
@@ -1,19 +1,20 @@
 -- set innodb lock wait timeout
 SET SESSION innodb_lock_wait_timeout = 7200;
 
-CREATE TABLE sys_notification_config(
-                                        `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
-                                        `organization_id` VARCHAR(50) NOT NULL   COMMENT '组织id' ,
-                                        `type` VARCHAR(64) NOT NULL   COMMENT '通知类型' ,
-                                        `value` TEXT    COMMENT '通知配置值' ,
-                                        PRIMARY KEY (id)
-)  COMMENT = '消息通知配置'
+CREATE TABLE sys_notification_config
+(
+    `id`              VARCHAR(32) NOT NULL COMMENT 'id',
+    `organization_id` VARCHAR(50) NOT NULL COMMENT '组织id',
+    `type`            VARCHAR(64) NOT NULL COMMENT '通知类型',
+    `value`           TEXT COMMENT '通知配置值',
+    PRIMARY KEY (id)
+) COMMENT = '消息通知配置'
 ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_organization_id ON sys_notification_config(organization_id ASC);
-CREATE INDEX idx_type ON sys_notification_config(type ASC);
+CREATE INDEX idx_organization_id ON sys_notification_config (organization_id ASC);
+CREATE INDEX idx_type ON sys_notification_config (type ASC);
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
