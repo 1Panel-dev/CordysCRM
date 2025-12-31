f9304eea85dcd35ccfd3313b217abf683c7f1429
guoyuqi <xiaomeinvG@126.com>
2025-12-31 14:25:43 +0800

    refactor: refactor messageTaskConfig save and get service

--- backend/crm/src/main/java/cn/cordys/crm/system/controller/MessageTaskController.java
+++ backend/crm/src/main/java/cn/cordys/crm/system/controller/MessageTaskController.java
@@ -8,6 +8,7 @@ import cn.cordys.context.OrganizationContext;
 import cn.cordys.crm.system.domain.MessageTask;
 import cn.cordys.crm.system.dto.MessageTaskConfigDTO;
 import cn.cordys.crm.system.dto.request.MessageTaskBatchRequest;
+import cn.cordys.crm.system.dto.request.MessageTaskConfigRequest;
 import cn.cordys.crm.system.dto.request.MessageTaskRequest;
 import cn.cordys.crm.system.dto.response.MessageTaskDTO;
 import cn.cordys.crm.system.service.MessageTaskService;
@@ -50,11 +51,11 @@ public class MessageTaskController {
     }
 
     //获取消息配置的config
-    @GetMapping("get/config")
+    @PostMapping("config/query")
     @Operation(summary = "项目管理-消息管理-消息设置-获取消息配置")
     @RequiresPermissions(PermissionConstants.SYSTEM_NOTICE_READ)
-    public MessageTaskConfigDTO getMessageConfig(@RequestParam String module, @RequestParam String event) {
-        return messageTaskService.getMessageConfig(module, event, OrganizationContext.getOrganizationId());
+    public MessageTaskConfigDTO getMessageConfig(@Validated @RequestBody MessageTaskConfigRequest request) {
+        return messageTaskService.getMessageConfig(request.getModule(), request.getEvent(), OrganizationContext.getOrganizationId());
     }
 
 
