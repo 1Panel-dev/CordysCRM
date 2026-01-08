74f2a1b376a58859fb04ba7004fa1f02691fb32b
AgAngle <1323481023@qq.com>
2026-01-08 14:21:05 +0800

    feat: init invoice permission

--- backend/crm/src/main/resources/migration/1.5.0/dml/V1.5.0_2_3__permission.sql
+++ backend/crm/src/main/resources/migration/1.5.0/dml/V1.5.0_2_3__permission.sql
@@ -37,4 +37,39 @@ VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_PAYMENT_RECORD:IMPORT');
 INSERT INTO sys_role_permission (id, role_id, permission_id)
 VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_PAYMENT_RECORD:EXPORT');
 
+
+-- Init invoice permissions
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:READ');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:ADD');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:UPDATE');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:DELETE');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:EXPORT');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'org_admin', 'CONTRACT_INVOICE:APPROVAL');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_manager', 'CONTRACT_INVOICE:READ');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_manager', 'CONTRACT_INVOICE:ADD');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_manager', 'CONTRACT_INVOICE:UPDATE');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_manager', 'CONTRACT_INVOICE:DELETE');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_manager', 'CONTRACT_INVOICE:EXPORT');
+
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_INVOICE:READ');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_INVOICE:ADD');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_INVOICE:UPDATE');
+INSERT INTO sys_role_permission (id, role_id, permission_id)
+VALUES (UUID_SHORT(), 'sales_staff', 'CONTRACT_INVOICE:EXPORT');
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
