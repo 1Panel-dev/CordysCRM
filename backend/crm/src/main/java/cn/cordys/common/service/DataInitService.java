2b9c87fc738ee00941d77732bad342fec3d81391
song-cc-rock <changchang.song@fit2cloud.com>
2026-01-14 12:21:40 +0800

    feat: Option field ref

--- backend/crm/src/main/java/cn/cordys/common/service/DataInitService.java
+++ backend/crm/src/main/java/cn/cordys/common/service/DataInitService.java
@@ -60,7 +60,7 @@ public class DataInitService {
             initOneTime(moduleFormService::initUpgradeForm, "init.upgrade.form.v1.4.0");
             initOneTime(moduleFormService::initUpgradeForm, "init.upgrade.form.v1.5.0");
             initOneTime(moduleFormService::initExtFieldsByVer, "1.5.0", "init.ext.fields.v1.5.0");
-			initOneTime(moduleFormExtService::setOptionDefaultSourceForSelect, "set.select.option.source");
+			initOneTime(moduleFormExtService::setOptionDefaultSource, "set.select.option.source");
         } finally {
             lock.unlock();
         }
