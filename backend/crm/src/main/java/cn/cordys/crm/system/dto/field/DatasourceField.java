f02731509ba04efbf52423990576e7596c96de0b
song-cc-rock <changchang.song@fit2cloud.com>
2026-01-14 17:26:22 +0800

    feat: Datasource link prop

--- backend/crm/src/main/java/cn/cordys/crm/system/dto/field/DatasourceField.java
+++ backend/crm/src/main/java/cn/cordys/crm/system/dto/field/DatasourceField.java
@@ -4,6 +4,7 @@ import cn.cordys.common.constants.EnumValue;
 import cn.cordys.crm.system.constants.FieldSourceType;
 import cn.cordys.crm.system.dto.field.base.BaseField;
 import cn.cordys.crm.system.dto.form.base.LinkField;
+import cn.cordys.crm.system.dto.form.base.SourceLink;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -19,15 +20,20 @@ import java.util.Map;
 @JsonTypeName(value = "DATA_SOURCE")
 @EqualsAndHashCode(callSuper = true)
 public class DatasourceField extends BaseField {
+
     @EnumValue(enumClass = FieldSourceType.class)
     @Schema(description = "数据源类型")
     private String dataSourceType;
+
     @Schema(description = "过滤条件")
     private Map<String, Object> combineSearch;
+
 	@Schema(description = "显示字段")
 	private List<String> showFields;
+
 	@Schema(description = "引用的字段集合")
 	private List<BaseField> refFields;
+
 	@Schema(description = "联动字段集合")
-	private List<LinkField> linkFields;
+	private List<SourceLink> linkFields;
 }
