eedb8bdae6841267480d95b551465143add86300
song-cc-rock <changchang.song@fit2cloud.com>
2026-01-14 16:56:56 +0800

    feat: Source link fields

--- backend/crm/src/main/java/cn/cordys/crm/system/dto/field/DatasourceField.java
+++ backend/crm/src/main/java/cn/cordys/crm/system/dto/field/DatasourceField.java
@@ -3,6 +3,7 @@ package cn.cordys.crm.system.dto.field;
 import cn.cordys.common.constants.EnumValue;
 import cn.cordys.crm.system.constants.FieldSourceType;
 import cn.cordys.crm.system.dto.field.base.BaseField;
+import cn.cordys.crm.system.dto.form.base.LinkField;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -27,4 +28,6 @@ public class DatasourceField extends BaseField {
 	private List<String> showFields;
 	@Schema(description = "引用的字段集合")
 	private List<BaseField> refFields;
+	@Schema(description = "联动字段集合")
+	private List<LinkField> linkFields;
 }
