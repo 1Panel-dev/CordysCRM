e69370eaa8f70ed3187a571a62af32f476a5888b
ba1q1 <qi.bai@fit2cloud.com>
2026-01-16 14:07:29 +0800

    feat: invoice api

--- frontend/packages/web/src/views/contract/invoice/locale/en-US.ts
+++ frontend/packages/web/src/views/contract/invoice/locale/en-US.ts
@@ -3,4 +3,7 @@ export default {
   'invoice.all': 'All Invoices',
   'invoice.my': 'My Invoices',
   'invoice.depart': 'Department Invoices',
+  'invoice.batchDeleteTitle': 'Are you sure you want to delete {count} invoice records?',
+  'invoice.batchDelete':
+    'Invoices under review/revoked/not approved: Deletion does not affect the invoiced amount of the contract; Invoices approved: After deletion, the invoiced amount of the contract will be reduced by the amount of this invoice application, please operate with caution!',
 };
