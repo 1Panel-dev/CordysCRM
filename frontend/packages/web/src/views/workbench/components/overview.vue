dc9014b6cd44bf4a8d3e70505e183b56ac84f93c
ba1q1 <qi.bai@fit2cloud.com>
2026-01-20 11:50:36 +0800

    style: workbench number

--- frontend/packages/web/src/views/workbench/components/overview.vue
+++ frontend/packages/web/src/views/workbench/components/overview.vue
@@ -31,10 +31,12 @@
               }`"
               @click="goDetail(dim, defaultLeadData[dim])"
             >
-              {{ abbreviateNumber(defaultLeadData[dim].value, defaultLeadData[dim].unit).value }}
-              <span class="unit">
+              <div class="number">
+                {{ abbreviateNumber(defaultLeadData[dim].value, defaultLeadData[dim].unit).value }}
+              </div>
+              <div class="unit">
                 {{ abbreviateNumber(defaultLeadData[dim].value, defaultLeadData[dim].unit).unit }}
-              </span>
+              </div>
             </div>
           </countPopover>
         </div>
@@ -60,8 +62,10 @@
                 }`"
                 @click="goDetail(dim, item)"
               >
-                {{ abbreviateNumber(item.value, item.unit).value }}
-                <span class="unit">{{ abbreviateNumber(item.value, item.unit).unit }}</span>
+                <div class="number">
+                  {{ abbreviateNumber(item.value, item.unit).value }}
+                </div>
+                <div class="unit">{{ abbreviateNumber(item.value, item.unit).unit }}</div>
               </div>
             </countPopover>
           </div>
@@ -84,8 +88,10 @@
                 }`"
                 @click="goDetail(dim, item)"
               >
-                {{ abbreviateNumber(item.value, item.unit).value }}
-                <span class="unit">{{ abbreviateNumber(item.value, item.unit).unit }}</span>
+                <div class="number">
+                  {{ abbreviateNumber(item.value, item.unit).value }}
+                </div>
+                <div class="unit">{{ abbreviateNumber(item.value, item.unit).unit }}</div>
               </div>
             </countPopover>
             <div
@@ -469,10 +475,14 @@
         @apply flex-1;
       }
       .count {
-        font-size: 18px;
-        @apply flex items-baseline font-semibold;
+        @apply flex items-center font-semibold;
+        .number {
+          font-size: 18px;
+          line-height: 29px;
+        }
         .unit {
           font-size: 14px;
+          line-height: 29px;
         }
       }
       .analytics-last-time {
