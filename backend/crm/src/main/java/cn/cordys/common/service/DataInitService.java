83c12da50ebe438699110f5967c9c073dc8a599a
song-cc-rock <changchang.song@fit2cloud.com>
2026-01-29 10:02:52 +0800

    fix: Refresh plan field pos
    --bug=1065920@tapd-34675357 --user=宋昌昌 【合同】合同表单设置-初始化默认有的子表格，产品和产品单价的字段属性的权限未全部放开 https://www.tapd.cn/34675357/s/1837826

--- backend/crm/src/main/java/cn/cordys/common/service/DataInitService.java
+++ backend/crm/src/main/java/cn/cordys/common/service/DataInitService.java
@@ -60,6 +60,7 @@ public class DataInitService {
             initOneTime(moduleFormService::initUpgradeForm, "init.upgrade.form.v1.5.0");
             initOneTime(moduleFormService::initExtFieldsByVer, "1.5.0", "init.ext.fields.v1.5.0");
 			initOneTime(moduleFieldExtService::setDefaultOptionSource, "set.default.option.source");
+			initOneTime(moduleFieldExtService::refreshPlanFieldPos, "refresh.plan.field.pos");
         } finally {
             lock.unlock();
         }
