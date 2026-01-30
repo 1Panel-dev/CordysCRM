8ed76cdbb18ec6f468bfa2a2004a81c1241eb31d
ba1q1 <qi.bai@fit2cloud.com>
2026-01-30 17:30:42 +0800

    fix: show control rule and link fields

--- frontend/packages/web/src/hooks/useFormCreateApi.ts
+++ frontend/packages/web/src/hooks/useFormCreateApi.ts
@@ -302,7 +302,7 @@ export default function useFormCreateApi(props: FormCreateApiProps) {
           const controlField = fieldList.value.find((f) => f.id === ruleId);
           if (controlField) {
             // 处理显示规则
-            if (fieldShowControlMap.value[fieldId][ruleId].includes(value || formDetail.value[controlField.id])) {
+            if (fieldShowControlMap.value[fieldId][ruleId].includes(formDetail.value[controlField.id])) {
               field.show = true;
               break; // 满足显示规则就停止，因为只需要满足一个规则字段即显示
             } else {
