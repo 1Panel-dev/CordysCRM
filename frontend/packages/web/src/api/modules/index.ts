ce3df0210cdfd5a9976b248e626be89a00996745
teukkk <mengyuan.lv@fit2cloud.com>
2026-01-30 17:55:20 +0800

    feat: add advanced search switch

--- frontend/packages/web/src/api/modules/index.ts
+++ frontend/packages/web/src/api/modules/index.ts
@@ -578,6 +578,8 @@ export const {
   deleteOpportunity,
   resetSearchConfig,
   setTopNavListSort,
+  setDisplayAdvanced,
+  getAdvancedSwitch,
   previewAttachment,
   addOpportunityRule,
   updateCustomerPool,
