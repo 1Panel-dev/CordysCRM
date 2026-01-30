7eb26b35b5ecdf097e241256a5041068130259ae
song-cc-rock <changchang.song@fit2cloud.com>
2026-01-30 11:54:17 +0800

    feat: Switch advance search settings
    --task=1020782@tapd-34675357 --user=宋昌昌 【搜索】高级搜索功能增加开关 #688-后端 https://www.tapd.cn/34675357/s/1839189

--- backend/crm/src/main/resources/migration/1.5.1/dml/V1.5.1_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.5.1/dml/V1.5.1_2_1__data.sql
@@ -6,7 +6,7 @@ SET @customer_addID = UUID_SHORT();
 INSERT INTO sys_message_task (id, event, task_type, email_enable, sys_enable, organization_id, template, create_user, create_time, update_user, update_time)
     VALUE (@customer_addID, 'SYNC_ORGANIZATION_STRUCTURE', 'SYSTEM', false, true,'100001', null, 'admin', UNIX_TIMESTAMP() * 1000 + 2, 'admin', UNIX_TIMESTAMP() * 1000 + 2 );
 
-
-
+-- init advance search settings
+INSERT INTO sys_parameter value ('advance.search.setting', 'false', 'text');
 
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
