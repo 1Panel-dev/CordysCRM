317b20c23c3d07bfef29fd823c536ef45035b523
AgAngle <1323481023@qq.com>
2026-02-12 14:38:01 +0800

    feat: approval config
    --bug=1066194@tapd-34675357 --user=陈建星 【发票】从1.5升级到1.5.1，发票中的合同表单联动设置默认初始化设置的有联动，且联动条件为不存在 https://www.tapd.cn/34675357/s/1845162

--- backend/crm/src/main/resources/migration/1.5.2/dml/V1.5.2_2_1__data.sql
+++ backend/crm/src/main/resources/migration/1.5.2/dml/V1.5.2_2_1__data.sql
@@ -6,4 +6,9 @@ VALUES (UUID_SHORT(), 'area', false, '100001'),
        (UUID_SHORT(), 'scale', false, '100001'),
        (UUID_SHORT(), 'industry', false, '100001');
 
+INSERT INTO sys_dict_config (module,organization_id,enabled)
+VALUES  ('QUOTATION_APPROVAL','100001',1),
+        ('CONTRACT_APPROVAL','100001',1),
+        ('INVOICE_APPROVAL','100001',1);
+
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
