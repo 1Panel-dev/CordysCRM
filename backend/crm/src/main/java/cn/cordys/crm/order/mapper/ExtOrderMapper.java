92e7cc41e38a809a2f81d66f97331c68df01f8c8
WangXu10 <xu.wang@fit2cloud.com>
2026-03-04 17:36:28 +0800

    feat: order stage config

--- backend/crm/src/main/java/cn/cordys/crm/order/mapper/ExtOrderMapper.java
+++ backend/crm/src/main/java/cn/cordys/crm/order/mapper/ExtOrderMapper.java
@@ -19,4 +19,6 @@ public interface ExtOrderMapper {
     List<OrderListResponse> getListByIds(@Param("ids") List<String> ids, @Param("userId") String userId, @Param("orgId") String orgId, @Param("dataPermission") DeptDataPermissionDTO deptDataPermission);
 
     void updateStatus(@Param("id") String id, @Param("status") String status, @Param("userId") String userId, @Param("updateTime") long updateTime);
+
+    int countByStage(@Param("stageId") String id);
 }
