44f453d566b1e11bc6dcace5bf0e709c2fbb4d48
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-05 18:16:00 +0800

    Revert: simplify Dockerfile and start script for application startup"
    This reverts commit d34c149e6aa6dec858a8d20c7192738ab72b9e15.

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -2,6 +2,8 @@
 
 bash /shells/init-directories.sh
 
+export JAVA_CLASSPATH=/app:/app/lib/*
+export JAVA_MAIN_CLASS=cn.cordys.Application
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -jar /app/cordys-crm.jar
+sh /deployments/run-java.sh
