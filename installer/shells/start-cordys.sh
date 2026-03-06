619e1c07300917e0b46c56c0a1ccfd992e859441
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-06 14:05:44 +0800

    refactor: update startup script to use Spring Boot JarLauncher for execution

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -2,7 +2,6 @@
 
 bash /shells/init-directories.sh
 
-export JAVA_CLASSPATH=/app:/app/lib/*
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -cp /app:/app/lib/* -jar /app/cordys-crm.jar
+exec java ${JAVA_OPTIONS} -cp /app/cordys-crm.jar:/app/lib/* org.springframework.boot.loader.JarLauncher
