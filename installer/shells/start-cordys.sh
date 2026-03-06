844f2c5d37dadd02a4a37868183767ad1fd08c3e
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-06 14:33:40 +0800

    refactor: update Dockerfile and startup script for frontend build integration

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -2,6 +2,8 @@
 
 bash /shells/init-directories.sh
 
+export JAVA_CLASSPATH=/app:/app/lib/*
+export JAVA_MAIN_CLASS=cn.cordys.Application
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -cp "/app/cordys-crm.jar:/app/lib/*" org.springframework.boot.loader.JarLauncher
+sh /deployments/run-java.sh
