de1e2da86cdd19c1716090975cfc6e10edd66b39
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-06 12:12:01 +0800

    refactor: update startup script to use jar execution with loader path

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -2,7 +2,6 @@
 
 bash /shells/init-directories.sh
 
-MAIN_CLASS="cn.cordys.Application"
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -cp "/app/cordys-crm.jar:/app/lib/*" ${MAIN_CLASS}
+exec java ${JAVA_OPTIONS} -Dloader.path=/app/lib -jar /app/cordys-crm.jar
