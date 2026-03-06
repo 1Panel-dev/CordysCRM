205e479554b0b1d1ece9a37eca7a6ef365360c18
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-06 14:19:36 +0800

    refactor: update startup script to quote classpath for jar execution

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -4,4 +4,4 @@ bash /shells/init-directories.sh
 
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -cp /app/cordys-crm.jar:/app/lib/* org.springframework.boot.loader.JarLauncher
+exec java ${JAVA_OPTIONS} -cp "/app/cordys-crm.jar:/app/lib/*" org.springframework.boot.loader.JarLauncher
