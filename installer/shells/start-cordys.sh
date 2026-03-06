faa6d094718861b2394672f5abbaf7b430a5c801
zhao <70558837+fit2-zhao@users.noreply.github.com>
2026-03-06 13:32:32 +0800

    refactor: update startup script to set JAVA_CLASSPATH and simplify execution command

--- installer/shells/start-cordys.sh
+++ installer/shells/start-cordys.sh
@@ -2,6 +2,7 @@
 
 bash /shells/init-directories.sh
 
+export JAVA_CLASSPATH=/app:/app/lib/*
 export CRM_VERSION=`cat /tmp/CRM_VERSION`
 
-exec java ${JAVA_OPTIONS} -Dloader.path=/app/lib -jar /app/cordys-crm.jar
+exec java ${JAVA_OPTIONS} -cp /app:/app/lib/* -jar /app/cordys-crm.jar
