d1f6ac40b25e0b412829c43e9b22bfad271cf9a3
teukkk <mengyuan.lv@fit2cloud.com>
2026-03-13 17:41:44 +0800

    fix: update order stage api

--- frontend/packages/web/src/views/order/locale/en-US.ts
+++ frontend/packages/web/src/views/order/locale/en-US.ts
@@ -6,4 +6,5 @@ export default {
   'order.depart': 'Department Orders',
   'order.my': 'My Orders',
   'order.deleteContent': 'The current order has been associated with the contract and cannot be deleted!',
+  'order.exportingPdf': 'Order PDF export in progress ...',
 };
