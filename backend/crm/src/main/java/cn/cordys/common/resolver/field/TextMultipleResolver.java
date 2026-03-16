27a417c94ba58bf916684b0a22964d9c4f7e882f
song-cc-rock <changchang.song@fit2cloud.com>
2026-03-16 13:56:04 +0800

    fix: Keep the order of the text multiple
    --bug=1066891@tapd-34675357 --user=宋昌昌 【线索】线索表单联动-设置标签填充多选/下拉多选，标签排序打乱了 https://www.tapd.cn/34675357/s/1868423

--- backend/crm/src/main/java/cn/cordys/common/resolver/field/TextMultipleResolver.java
+++ backend/crm/src/main/java/cn/cordys/common/resolver/field/TextMultipleResolver.java
@@ -47,24 +47,22 @@ public class TextMultipleResolver extends AbstractModuleFieldResolver<InputMulti
             return null;
         }
         List<String> textList = parseFakeJsonArray(text);
-        Set<String> correctTags = getCorrectFormatInput(textList);
-        return CollectionUtils.isEmpty(correctTags) ? null : new ArrayList<>(correctTags);
+        List<String> correctTags = getCorrectFormatInput(textList);
+        return CollectionUtils.isEmpty(correctTags) ? null : correctTags;
     }
 
     /**
      * 要求标签最多10个, 且每个标签长度不超过64个字符
-     *
      * @param textList 标签列表
-     *
      * @return 符合要求的标签集合
      */
-    public Set<String> getCorrectFormatInput(List<String> textList) {
+    public List<String> getCorrectFormatInput(List<String> textList) {
         return textList.stream()
                 .map(String::trim)
                 .filter(StringUtils::isNotBlank)
                 .filter(text -> text.length() <= 64)
                 .distinct()
                 .limit(10)
-                .collect(Collectors.toSet());
+                .toList();
     }
 }
