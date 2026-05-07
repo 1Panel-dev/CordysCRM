314b7760c287d72ac954ca68f48935044e31f88f
song-cc-rock <changchang.song@fit2cloud.com>
2026-05-07 11:52:16 +0800

    feat: Approval detail

--- backend/crm/src/main/java/cn/cordys/crm/approval/controller/ResourceApprovalController.java
+++ backend/crm/src/main/java/cn/cordys/crm/approval/controller/ResourceApprovalController.java
@@ -1,6 +1,8 @@
 package cn.cordys.crm.approval.controller;
 
+import cn.cordys.crm.approval.dto.ApprovalInstanceDetail;
 import cn.cordys.crm.approval.dto.response.ResourceApprovalResponse;
+import cn.cordys.crm.approval.service.ApprovalInstanceService;
 import cn.cordys.crm.approval.service.ResourceApprovalService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,23 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 资源审核相关
- * <p>
- * 提供资源目前审核状态以及审核信息的功能
- * </p>
+ * 资源审批相关
  */
 @RestController
 @RequestMapping("/resource-approval")
-@Tag(name = "资源审核状态")
+@Tag(name = "资源审批")
 public class ResourceApprovalController {
 
     @Resource
     private ResourceApprovalService resourceApprovalService;
+	@Resource
+	private ApprovalInstanceService approvalInstanceService;
 
     @GetMapping("/detail/{resourceId}")
-    @Operation(summary = "资源审核状态-查找资源当前审核状态")
+    @Operation(summary = "当前资源审批详情")
     public ResourceApprovalResponse resourceDetail(@PathVariable String resourceId) {
         return resourceApprovalService.resourceDetail(resourceId);
     }
 
+	@GetMapping("/record-detail/{resourceId}")
+	@Operation(summary = "审批记录详情")
+	public ApprovalInstanceDetail getDetail(@PathVariable String resourceId) {
+		return approvalInstanceService.getLatestApprovalInstanceDetail(resourceId);
+	}
 }
