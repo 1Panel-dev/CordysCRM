fe3439b084d85e62d3657a6539e54e5037d70f02
AgAngle <1323481023@qq.com>
2026-05-11 11:45:03 +0800

    refactor: approval_flow_version

--- backend/crm/src/main/resources/migration/1.7.0/ddl/V1.7.0_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.7.0/ddl/V1.7.0_2__ga_ddl.sql
@@ -3,42 +3,51 @@ SET SESSION innodb_lock_wait_timeout = 7200;
 
 CREATE TABLE approval_flow(
   `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
+  `current_version_id` VARCHAR(32) NOT NULL   COMMENT '当前版本ID' ,
   `number` VARCHAR(50) NOT NULL   COMMENT '流程编码;流程编码，自增，格式：CTR-APV-001（合同），INV-APV-001（发票），ORD-APV-001（订单）' ,
   `name` VARCHAR(200) NOT NULL   COMMENT '流程名称;流程名称' ,
   `form_type` VARCHAR(50) NOT NULL   COMMENT '表单类型;表单类型：QUOTATION(报价)、CONTRACT(合同)、INVOICE(发票)、ORDER(订单)' ,
-  `create_execute` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '新建时执行;新建时是否执行审批流' ,
-  `update_execute` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '编辑时执行;编辑时是否执行审批流' ,
   `enable` TINYINT(1) NOT NULL  DEFAULT 1 COMMENT '启用状态;启用状态：0-禁用，1-启用' ,
-  `submitter_can_revoke` TINYINT(1) NOT NULL  DEFAULT 1 COMMENT '允许提交人撤销;允许提交人撤销审批中的申请' ,
-  `allow_batch_process` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许批量处理;允许审批人批量处理此流程的多个任务' ,
-  `allow_withdraw` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许撤回;允许审批人撤回审批' ,
-  `allow_add_sign` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许加签' ,
-  `duplicate_approver_rule` VARCHAR(20) NOT NULL  DEFAULT 'FIRST_ONLY' COMMENT '重复审批人：FIRST_ONLY/SEQUENTIAL_ALL/EACH' ,
-  `require_comment` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '是否必须填写审批意见' ,
   `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
   `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
   `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
   `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
   `organization_id` VARCHAR(32) NOT NULL   COMMENT '组织id' ,
+  `description` VARCHAR(3000)    COMMENT '流程描述' ,
   PRIMARY KEY (id)
-)  COMMENT = '审批流表'
+)  COMMENT = '审批流主表'
 ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE TABLE approval_flow_blob(
-   `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
-   `status_permissions` text NOT NULL   COMMENT '状态权限配置（JSON格式）' ,
-   `description` VARCHAR(3000)    COMMENT '流程描述' ,
-   PRIMARY KEY (id)
-)  COMMENT = '审批流大字段表'
+CREATE TABLE approval_flow_version(
+    `id` VARCHAR(32) NOT NULL   COMMENT 'ID' ,
+    `flow_id` VARCHAR(32) NOT NULL   COMMENT '审批流ID' ,
+    `create_execute` TINYINT(1) NOT NULL  DEFAULT 1 COMMENT '新建时执行;新建时是否执行审批流' ,
+    `update_execute` TINYINT(1) NOT NULL  DEFAULT 1 COMMENT '编辑时执行;编辑时是否执行审批流' ,
+    `submitter_can_revoke` TINYINT(1) NOT NULL  DEFAULT 1 COMMENT '允许提交人撤销;允许提交人撤销审批中的申请' ,
+    `allow_batch_process` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许批量处理;允许审批人批量处理此流程的多个任务' ,
+    `allow_withdraw` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许撤回;允许审批人撤回审批' ,
+    `allow_add_sign` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '允许加签' ,
+    `duplicate_approver_rule` VARCHAR(20) NOT NULL  DEFAULT 'FIRST_ONLY' COMMENT '重复审批人：FIRST_ONLY/SEQUENTIAL_ALL/EACH' ,
+    `require_comment` TINYINT(1) NOT NULL  DEFAULT 0 COMMENT '是否必须填写审批意见' ,
+    `create_time` BIGINT NOT NULL   COMMENT '创建时间' ,
+    `update_time` BIGINT NOT NULL   COMMENT '更新时间' ,
+    `create_user` VARCHAR(32) NOT NULL   COMMENT '创建人' ,
+    `update_user` VARCHAR(32) NOT NULL   COMMENT '更新人' ,
+    `organization_id` VARCHAR(32) NOT NULL   COMMENT '组织id' ,
+    `status_permissions` VARCHAR(4000) NOT NULL   COMMENT '状态权限配置（JSON格式）' ,
+    PRIMARY KEY (id)
+)  COMMENT = '审批流版本表'
 ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
+
+CREATE INDEX idx_flow_id ON approval_flow_version(flow_id ASC);
 
 CREATE TABLE approval_node(
   `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
-  `flow_id` VARCHAR(32) NOT NULL   COMMENT '流程ID' ,
+  `flow_version_id` VARCHAR(32) NOT NULL   COMMENT '审批流版本ID' ,
   `name` VARCHAR(200) NOT NULL   COMMENT '节点名称' ,
   `node_type` VARCHAR(50) NOT NULL   COMMENT '节点类型：START\CONDITION\DEFAULT\END' ,
   `sort` INT(11) NOT NULL  DEFAULT 0 COMMENT '排序序号' ,
@@ -48,11 +57,11 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_flow_id ON approval_node(flow_id ASC);
+CREATE INDEX idx_flow_version_id ON approval_node(flow_version_id ASC);
 
 CREATE TABLE approval_node_approver(
    `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
-   `flow_id` VARCHAR(32) NOT NULL   COMMENT '流程ID;流程ID' ,
+   `flow_version_id` VARCHAR(32) NOT NULL   COMMENT '审批流版本ID' ,
    `approval_type` VARCHAR(20) NOT NULL  DEFAULT 'AUTO_PASS' COMMENT '审批类型;审批类型：MANUAL(人工审批)、AUTO_PASS(自动通过)、AUTO_REJECT(自动拒绝)' ,
    `multi_approver_mode` VARCHAR(20) NOT NULL   COMMENT '多人审批方式;多人审批方式：ALL(会签)/ANY(或签)/SEQUENTIAL(依次审批)' ,
    `empty_approver_action` VARCHAR(20) NOT NULL  DEFAULT 'AUTO_PASS' COMMENT '审批人为空时动作：AUTO_PASS(自动通过)/ASSIGN_SPECIFIC(指定人员审批)/ASSIGN_ADMIN(转交给审批管理员)' ,
@@ -71,11 +80,11 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_flow_id ON approval_node_approver(flow_id ASC);
+CREATE INDEX idx_flow_version_id ON approval_node_approver(flow_version_id ASC);
 
 CREATE TABLE approval_node_condition(
     `id` VARCHAR(32) NOT NULL   COMMENT 'id' ,
-    `flow_id` VARCHAR(32) NOT NULL   COMMENT '流程ID' ,
+    `flow_version_id` VARCHAR(32) NOT NULL COMMENT '审批流版本ID' ,
     `condition_config` VARCHAR(4000)  COMMENT '条件配置JSON' ,
     PRIMARY KEY (id)
 )  COMMENT = '条件节点配置表'
@@ -83,11 +92,11 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_flow_id ON approval_node_condition(flow_id ASC);
+CREATE INDEX idx_flow_version_id ON approval_node_condition(flow_version_id ASC);
 
 CREATE TABLE approval_node_link(
    `id` VARCHAR(32) NOT NULL   COMMENT '主键' ,
-   `flow_id` VARCHAR(32) NOT NULL   COMMENT '流程ID' ,
+   `flow_version_id` VARCHAR(32) NOT NULL   COMMENT '审批流版本ID' ,
    `from_node_id` VARCHAR(32) NOT NULL   COMMENT '源节点ID' ,
    `to_node_id` VARCHAR(32) NOT NULL   COMMENT '目标节点ID' ,
    `sort` INT NOT NULL  DEFAULT 0 COMMENT '分支评估顺序' ,
@@ -97,11 +106,11 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_flow_id_from_id ON approval_node_link(flow_id ASC,from_node_id ASC);
+CREATE INDEX idx_flow_version_id_from_id ON approval_node_link(flow_version_id ASC,from_node_id ASC);
 
 CREATE TABLE approval_instance(
     `id` VARCHAR(32) NOT NULL COMMENT 'ID' ,
-    `flow_id` VARCHAR(32) NOT NULL   COMMENT '流程ID' ,
+    `flow_version_id` VARCHAR(32) NOT NULL   COMMENT '审批流版本ID' ,
     `type` VARCHAR(20) NOT NULL   COMMENT '表单类型' ,
     `resource_id` VARCHAR(32) NOT NULL   COMMENT '审批的业务数据ID' ,
     `submitter_id` VARCHAR(32) NOT NULL   COMMENT '提交人ID' ,
@@ -120,7 +129,7 @@ CREATE TABLE approval_instance(
     DEFAULT CHARSET = utf8mb4
     COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_flow_id ON approval_instance(flow_id ASC);
+CREATE INDEX idx_flow_version_id ON approval_instance(flow_version_id ASC);
 CREATE INDEX idx_resource_id ON approval_instance(resource_id ASC);
 CREATE INDEX idx_submitter_id ON approval_instance(submitter_id ASC);
 
