2c3496f2596c881c8feb76b3dd904fcd6113b88c
song-cc-rock <changchang.song@fit2cloud.com>
2026-05-21 14:08:55 +0800

    fix: Sign task error

--- backend/crm/src/main/java/cn/cordys/crm/approval/dto/request/ApprovalAddSignRequest.java
+++ backend/crm/src/main/java/cn/cordys/crm/approval/dto/request/ApprovalAddSignRequest.java
@@ -2,6 +2,7 @@ package cn.cordys.crm.approval.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class ApprovalAddSignRequest extends ApprovalActionRequest {
     @Schema(description = "加签方式 BEFORE: 在我之前，AFTER: 在我之后")
     private String type;
 
+	@NotEmpty
 	@Schema(description = "加签审批人")
-	private String signApprover;
+	private List<String> signApprover;
 }
