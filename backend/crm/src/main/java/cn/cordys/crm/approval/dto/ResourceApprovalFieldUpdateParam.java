9aa799b0fc3a749b971787bfdbc4a174847ed233
song-cc-rock <changchang.song@fit2cloud.com>
2026-05-25 11:04:55 +0800

    fix: AApproval post update error

--- backend/crm/src/main/java/cn/cordys/crm/approval/dto/ResourceApprovalFieldUpdateParam.java
+++ backend/crm/src/main/java/cn/cordys/crm/approval/dto/ResourceApprovalFieldUpdateParam.java
@@ -1,6 +1,9 @@
 package cn.cordys.crm.approval.dto;
 
+import cn.cordys.common.util.JSON;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class ResourceApprovalFieldUpdateParam {
@@ -11,9 +14,19 @@ public class ResourceApprovalFieldUpdateParam {
 	/**
 	 * 字段更新值
 	 */
-	private String fieldValue;
+	private Object fieldValue;
 	/**
 	 * 是否开启
 	 */
 	private boolean enable;
+
+	public Object getFieldValue() {
+		if (fieldValue == null) {
+			return null;
+		}
+		if (fieldValue instanceof List<?>) {
+			return JSON.toJSONString(fieldValue);
+		}
+		return fieldValue;
+	}
 }
