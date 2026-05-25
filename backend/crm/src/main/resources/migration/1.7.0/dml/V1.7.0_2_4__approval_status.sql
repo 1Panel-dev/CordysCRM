1b2d246e738a350c0c692f74f1350a87b8bf1828
song-cc-rock <changchang.song@fit2cloud.com>
2026-05-25 17:54:43 +0800

    chore: Remove unuse flyway

--- backend/crm/src/main/resources/migration/1.7.0/dml/V1.7.0_2_4__approval_status.sql
+++
@@ -1,7 +0,0 @@
--- set innodb lock wait timeout
-SET SESSION innodb_lock_wait_timeout = 7200;
-
--- handle order his approval status
-update sales_order set approval_status = 'NONE';
-
-SET SESSION innodb_lock_wait_timeout = DEFAULT;
