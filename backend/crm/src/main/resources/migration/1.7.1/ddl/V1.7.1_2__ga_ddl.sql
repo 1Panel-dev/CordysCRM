5b60720adbd2ca8570022afa2d1ad4e2f797aa75
WangXu10 <xu.wang@fit2cloud.com>
2026-06-03 16:18:47 +0800

    refactor: flyway sql

--- backend/crm/src/main/resources/migration/1.7.1/ddl/V1.7.1_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.7.1/ddl/V1.7.1_2__ga_ddl.sql
@@ -5,7 +5,22 @@ SET SESSION innodb_lock_wait_timeout = 7200;
 ALTER TABLE contract MODIFY COLUMN start_time bigint NULL;
 ALTER TABLE contract MODIFY COLUMN end_time bigint NULL;
 
-ALTER TABLE business_title ADD COLUMN company_number BIGINT NOT NULL AUTO_INCREMENT UNIQUE;
+SET @sql = (
+    SELECT IF(
+        EXISTS (
+            SELECT 1
+            FROM information_schema.COLUMNS
+            WHERE TABLE_SCHEMA = DATABASE()
+              AND TABLE_NAME = 'business_title'
+              AND COLUMN_NAME = 'company_number'
+        ),
+        'SELECT 1',
+        'ALTER TABLE business_title ADD COLUMN company_number BIGINT NOT NULL AUTO_INCREMENT UNIQUE COMMENT ''公司编号'''
+    )
+);
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
