5f2432337445a902b5ac1e5201f59229fe63480d
Sombody-JIAN <herman.song.yang@gmail.com>
2026-06-09 10:50:24 +0800

    fix: user role related param

--- backend/crm/src/main/java/cn/cordys/crm/system/service/UserRoleService.java
+++ backend/crm/src/main/java/cn/cordys/crm/system/service/UserRoleService.java
@@ -194,13 +194,13 @@ public class UserRoleService {
                     .filter(userId -> userNameMap.get(userId) != null)
                     .toList();
 
-            List<String> currentRoleUserIds = extUserRoleMapper.getUserIdsByRoleIds(List.of(request.getCustomFormRole()));
+            List<String> currentRoleUserIds = extUserRoleMapper.getUserIdsByRoleIds(List.of(request.getRoleId()));
             // 去除已关联的用户，添加未关联的用户
             List<UserRole> userRoles = ListUtils.subtract(subUserIds, currentRoleUserIds).stream()
                     .map(userId -> {
                         UserRole userRole = new UserRole();
                         userRole.setUserId(userId);
-                        userRole.setRoleId(request.getCustomFormRole());
+                        userRole.setRoleId(request.getRoleId());
                         userRole.setId(IDGenerator.nextStr());
                         userRole.setCreateUser(operator);
                         userRole.setUpdateUser(operator);
@@ -211,7 +211,7 @@ public class UserRoleService {
                     .collect(Collectors.toList());
             userRoleMapper.batchInsert(userRoles);
 
-            Role role = roleMapper.selectByPrimaryKey(request.getCustomFormRole());
+            Role role = roleMapper.selectByPrimaryKey(request.getRoleId());
             role.setName(roleService.translateInternalRole(role.getName()));
             List<LogDTO> logs = new ArrayList<>();
             userRoles.forEach(u -> {
