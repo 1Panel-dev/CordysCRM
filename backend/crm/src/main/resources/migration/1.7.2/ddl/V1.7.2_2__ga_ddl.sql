5f53a1e4ac74f7f054845651173f35895d690a3a
WangXu10 <xu.wang@fit2cloud.com>
2026-06-17 10:18:18 +0800

    refactor: stage advanced config table field

--- backend/crm/src/main/resources/migration/1.7.2/ddl/V1.7.2_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.7.2/ddl/V1.7.2_2__ga_ddl.sql
@@ -18,7 +18,7 @@ CREATE TABLE stage_advanced_config
     `target_id`       VARCHAR(32) NOT NULL COMMENT '目标id',
     `enable`          TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否允许流转',
     `field_config`    TEXT(255) COMMENT '字段配置',
-    `type`            VARCHAR(20) COMMENT '类型',
+    `module_type`     VARCHAR(20) COMMENT '模块类型(order-订单/contract-合同)',
     `organization_id` VARCHAR(32) NOT NULL COMMENT '组织id',
     `create_time`     BIGINT      NOT NULL COMMENT '创建时间',
     `update_time`     BIGINT      NOT NULL COMMENT '更新时间',
@@ -30,7 +30,7 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_general_ci;
 
-CREATE INDEX idx_type ON stage_advanced_config (type ASC);
+CREATE INDEX idx_type ON stage_advanced_config (module_type ASC);
 
 
 ALTER TABLE sales_order_stage_config ADD COLUMN circulation_type VARCHAR(50) DEFAULT 'NORMAL' COMMENT '流转类型(NORMAL-普通，ADVANCED-高级)';
