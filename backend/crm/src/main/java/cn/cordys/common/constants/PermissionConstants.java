ba9649bba7852995460b9f1f5c8d2e0cc740aded
WangXu10 <xu.wang@fit2cloud.com>
2026-07-10 15:37:48 +0800

    feat: product export

--- backend/crm/src/main/java/cn/cordys/common/constants/PermissionConstants.java
+++ backend/crm/src/main/java/cn/cordys/common/constants/PermissionConstants.java
@@ -91,6 +91,7 @@ public class PermissionConstants {
     public static final String PRODUCT_MANAGEMENT_UPDATE = "PRODUCT_MANAGEMENT:UPDATE";
     public static final String PRODUCT_MANAGEMENT_DELETE = "PRODUCT_MANAGEMENT:DELETE";
     public static final String PRODUCT_MANAGEMENT_IMPORT = "PRODUCT_MANAGEMENT:IMPORT";
+    public static final String PRODUCT_MANAGEMENT_EXPORT = "PRODUCT_MANAGEMENT:EXPORT";
     /*------ end: PRODUCT_MANAGEMENT ------*/
 
     /*------ start: OPPORTUNITY_MANAGEMENT ------*/
