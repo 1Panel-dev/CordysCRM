34bcb8eda9db81874454bb0d530f7d490ca077b0
WangXu10 <xu.wang@fit2cloud.com>
2026-07-14 14:51:53 +0800

    fix: opportunity update
    --bug=1071548@tapd-34675357 --user=王旭 【商机】github#2911对商机进行更新修改，更新者和更新时间不同步 https://www.tapd.cn/34675357/s/1983297

--- backend/crm/src/main/java/cn/cordys/crm/opportunity/service/OpportunityService.java
+++ backend/crm/src/main/java/cn/cordys/crm/opportunity/service/OpportunityService.java
@@ -351,7 +351,7 @@ public class OpportunityService {
                 // 更新模块字段
                 updateModuleField(updateOpportunity, request.getModuleFields(), orgId, userId);
             }
-            extOpportunityMapper.updateIncludeNullById(updateOpportunity);
+            extOpportunityMapper.updateOpportunity(updateOpportunity);
             baseService.handleUpdateLog(oldOpportunity, newOpportunity, originCustomerFields, request.getModuleFields(), oldOpportunity.getId(), oldOpportunity.getName());
         }, () -> {
             throw new GenericException("opportunity_not_found");
