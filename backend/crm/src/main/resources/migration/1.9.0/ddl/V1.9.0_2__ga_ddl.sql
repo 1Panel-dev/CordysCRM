1c1bbd8fd2e61ddfc13087e71324f1ede84acc57
WangXu10 <xu.wang@fit2cloud.com>
2026-07-31 09:37:58 +0800

    refactor: custom form data import support sub table

--- backend/crm/src/main/resources/migration/1.9.0/ddl/V1.9.0_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.9.0/ddl/V1.9.0_2__ga_ddl.sql
@@ -10,10 +10,20 @@ ALTER TABLE contract_invoice_field_blob ADD COLUMN ref_sub_id VARCHAR(32) NULL C
 ALTER TABLE contract_invoice_field_blob ADD COLUMN row_id VARCHAR(32) NULL COMMENT '子表格行实例ID;行实例数据ID';
 ALTER TABLE contract_invoice_field_blob ADD COLUMN biz_id VARCHAR(32) NULL COMMENT '唯一业务行ID';
 
+ALTER TABLE custom_form_data_field ADD COLUMN ref_sub_id VARCHAR(32) NULL COMMENT '引用子表格ID;关联的子表格字段ID';
+ALTER TABLE custom_form_data_field ADD COLUMN row_id VARCHAR(32) NULL COMMENT '子表格行实例ID;行实例数据ID';
+ALTER TABLE custom_form_data_field ADD COLUMN biz_id VARCHAR(32) NULL COMMENT '唯一业务行ID';
+
+ALTER TABLE custom_form_data_field_blob ADD COLUMN ref_sub_id VARCHAR(32) NULL COMMENT '引用子表格ID;关联的子表格字段ID';
+ALTER TABLE custom_form_data_field_blob ADD COLUMN row_id VARCHAR(32) NULL COMMENT '子表格行实例ID;行实例数据ID';
+ALTER TABLE custom_form_data_field_blob ADD COLUMN biz_id VARCHAR(32) NULL COMMENT '唯一业务行ID';
 
 -- add unique index
 CREATE UNIQUE INDEX uk_contract_invoice_field_cell ON contract_field (resource_id, row_id, field_id);
 CREATE UNIQUE INDEX uk_contract_invoice_field_blob_cell ON contract_field_blob (resource_id, row_id, field_id);
+
+CREATE UNIQUE INDEX uk_custom_form_data_field_cell ON contract_field (resource_id, row_id, field_id);
+CREATE UNIQUE INDEX uk_custom_form_data_field_blob_cell ON contract_field_blob (resource_id, row_id, field_id);
 
 -- agent ddl
 CREATE TABLE agent_model
