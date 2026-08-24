2ca7b734b538ecb932ee0aa15ed69020a1d07f7a
song-cc-rock <changchang.song@fit2cloud.com>
2026-08-24 10:01:35 +0800

    feat: Agent execute log ddl

--- backend/crm/src/main/resources/migration/1.9.0/ddl/V1.9.0_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.9.0/ddl/V1.9.0_2__ga_ddl.sql
@@ -193,6 +193,8 @@ CREATE TABLE agent_term_discovery(
 CREATE TABLE agent_task_execute_log(
     `id` VARCHAR(32) NOT NULL   COMMENT 'ID' ,
     `task_id` VARCHAR(32) NOT NULL   COMMENT '任务ID' ,
+    `task_name` VARCHAR(255) NOT NULL   COMMENT '任务名称快照' ,
+    `organization_id` VARCHAR(32) NOT NULL   COMMENT '组织ID' ,
     `run_id` VARCHAR(100) NOT NULL   COMMENT '执行ID' ,
     `execute_time` BIGINT NOT NULL   COMMENT '执行时间' ,
     `execute_reason` VARCHAR(500) NOT NULL   COMMENT '触发原因' ,
