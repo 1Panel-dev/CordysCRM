e78c4330a5638fd479fb731542d7a8934ff1cdac
song-cc-rock <changchang.song@fit2cloud.com>
2026-09-03 11:40:57 +0800

    feat: Add agent message status column

--- backend/crm/src/main/resources/migration/1.9.1/ddl/V1.9.1_2__ga_ddl.sql
+++ backend/crm/src/main/resources/migration/1.9.1/ddl/V1.9.1_2__ga_ddl.sql
@@ -1,8 +1,12 @@
 -- set innodb lock wait timeout
 SET SESSION innodb_lock_wait_timeout = 7200;
 
--- API 请求地址列放宽到 255
+-- API URL 长度放宽到 255字符
 ALTER TABLE agent_model MODIFY COLUMN `api_url` VARCHAR(255) COMMENT 'API请求地址';
+
+-- 历史消息支持状态
+ALTER TABLE agent_message
+    ADD COLUMN `status` VARCHAR(20) NULL COMMENT '消息状态：done 已完成，stopped 已停止' AFTER `helpful`;
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
