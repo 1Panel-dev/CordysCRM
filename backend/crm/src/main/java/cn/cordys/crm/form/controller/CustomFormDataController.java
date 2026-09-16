06cf3e59b6f3bb0d608cc88c6481bea7f983075b
AgAngle <1323481023@qq.com>
2026-09-16 17:39:18 +0800

    fix: custom form data batch operate response
    --bug=1076418@tapd-34675357 --user=陈建星 【自定义表单】批量编辑结果为空 https://www.tapd.cn/34675357/s/2080614

--- backend/crm/src/main/java/cn/cordys/crm/form/controller/CustomFormDataController.java
+++ backend/crm/src/main/java/cn/cordys/crm/form/controller/CustomFormDataController.java
@@ -14,6 +14,7 @@ import cn.cordys.crm.form.dto.response.CustomFormDataListResponse;
 import cn.cordys.crm.form.service.CustomFormDataExportService;
 import cn.cordys.crm.form.service.CustomFormDataService;
 import cn.cordys.crm.system.constants.ExportConstants;
+import cn.cordys.crm.system.dto.response.BatchAffectReasonResponse;
 import cn.cordys.crm.system.dto.response.ImportResponse;
 import cn.cordys.security.SessionUtils;
 import io.swagger.v3.oas.annotations.Operation;
@@ -82,8 +83,8 @@ public class CustomFormDataController {
     @PostMapping("/batch/update")
     @Operation(summary = "批量更新表单数据")
     @CsPermission(PermissionConstants.CUSTOM_FORM_READ)
-    public void batchUpdate(@Validated @RequestBody CustomFormDataBatchUpdateRequest request) {
-        customFormDataService.batchUpdate(request, SessionUtils.getUserId(), OrganizationContext.getOrganizationId());
+    public BatchAffectReasonResponse batchUpdate(@Validated @RequestBody CustomFormDataBatchUpdateRequest request) {
+        return customFormDataService.batchUpdate(request, SessionUtils.getUserId(), OrganizationContext.getOrganizationId());
     }
 
     @PostMapping("/batch/delete")
