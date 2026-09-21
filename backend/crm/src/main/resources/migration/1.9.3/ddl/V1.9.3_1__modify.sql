c3daf5bb61bf4ce581705ce10b733098fddb759c
WangXu10 <xu.wang@fit2cloud.com>
2026-09-21 10:38:21 +0800

    fix: opportunity stage config

--- backend/crm/src/main/resources/migration/1.9.3/ddl/V1.9.3_1__modify.sql
+++ backend/crm/src/main/resources/migration/1.9.3/ddl/V1.9.3_1__modify.sql
@@ -2,6 +2,7 @@
 SET SESSION innodb_lock_wait_timeout = 7200;
 
 ALTER TABLE schedule ADD COLUMN update_user varchar(32) NOT NULL COMMENT '更新人';
+ALTER TABLE opportunity_stage_config ADD COLUMN circulation_type VARCHAR(50) DEFAULT 'NORMAL' COMMENT '流转类型(NORMAL-普通，ADVANCED-高级)';
 
 -- set innodb lock wait timeout to default
 SET SESSION innodb_lock_wait_timeout = DEFAULT;
